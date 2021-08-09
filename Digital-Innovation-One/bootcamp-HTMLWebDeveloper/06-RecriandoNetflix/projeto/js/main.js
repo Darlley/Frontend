f52f8b6319a81b87a7729c970d11ce668d0ae0dd
Darlley <darlleybrito@gmail.com>
2021-08-09 16:54:22 -0400

    tolkienflix

---
+++ Digital-Innovation-One/bootcamp-HTMLWebDeveloper/06-RecriandoNetflix/projeto/js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
