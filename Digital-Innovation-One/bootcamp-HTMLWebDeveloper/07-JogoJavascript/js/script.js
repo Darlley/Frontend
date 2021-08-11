4dbaba125584cb24d89affe0a66e8e23ad974055
Darlley <darlleybrito@gmail.com>
2021-08-11 17:06:18 -0400

    final version

--- Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
+++ Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
@@ -47,7 +47,7 @@ function iniciarJogo(){
     for(i=1; i<snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert('Game Over :(')
+            alert('Você perdeu :(')
         }
     }
 
