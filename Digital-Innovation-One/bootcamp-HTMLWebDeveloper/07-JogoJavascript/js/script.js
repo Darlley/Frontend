023a1247f017fddbe5267cd2d92f7b2860c4249d
Darlley <darlleybrito@gmail.com>
2021-08-11 16:02:09 -0400

    movimento unico

--- Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
+++ Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
@@ -6,17 +6,39 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 } 
+let direction = 'right';
 
 function criarBG(){
     context.fillStyle = 'lightgreen';
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarSnake(){
+function createSnake(){
     for(i=0; i<snake.length;i++){
         context.fillStyle = 'green';
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-criarBG();
-criarSnake();
+
+function iniciarJogo(){
+    criarBG();  
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == 'right') snakeX += box;
+    if(direction == 'left') snakeX -= box;
+    if(direction == 'up') snakeY -= box;
+    if(direction == 'down') snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100)
