78dcb6a3db75c5f947e9b6f53eaa181e8159d754
Darlley <darlleybrito@gmail.com>
2021-08-11 16:07:41 -0400

    movimento

--- Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
+++ Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
@@ -20,6 +20,15 @@ function createSnake(){
     }
 }
 
+// ao apertar uma tecla
+document.addEventListener('keydown', update);
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function iniciarJogo(){
     criarBG();  
     createSnake();
