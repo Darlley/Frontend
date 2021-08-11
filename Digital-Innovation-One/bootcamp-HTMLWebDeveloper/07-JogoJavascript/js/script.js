3ef04d2049801e5b3b5a90980b6b010ba380a159
Darlley <darlleybrito@gmail.com>
2021-08-11 16:33:36 -0400

    first version

--- Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
+++ Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
@@ -19,13 +19,13 @@ function criarBG(){
 
 function createSnake(){
     for(i=0; i<snake.length;i++){
-        context.fillStyle = '#781fd1';
+        context.fillStyle = 'black';
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawFood(){
-    context.fillStyle = 'black';
+    context.fillStyle = '#781fd1';
     context.fillRect(food.x, food.y, box, box);
 }
 
@@ -56,7 +56,12 @@ function iniciarJogo(){
     if(direction == 'up') snakeY -= box;
     if(direction == 'down') snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
