a6f0278fd04355b37b7ba65a6cea9219e3a11dbc
Darlley <darlleybrito@gmail.com>
2021-08-11 15:54:19 -0400

    snake e background

--- Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
+++ Digital-Innovation-One/bootcamp-HTMLWebDeveloper/07-JogoJavascript/js/script.js
@@ -1,10 +1,22 @@
 let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d');
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+} 
 
 function criarBG(){
     context.fillStyle = 'lightgreen';
-    context.fillReact(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarSnake(){
+    for(i=0; i<snake.length;i++){
+        context.fillStyle = 'green';
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
 criarBG();
+criarSnake();
