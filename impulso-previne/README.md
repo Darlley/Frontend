899a46bb91536f41d56c7d646b03d23741952f79
Darlley <systembrtt@gmail.com>
2021-10-02 16:07:44 -0400

    deploy desafio impulsogov

---
+++ impulso-previne/README.md
@@ -0,0 +1,1 @@
+# impulsogov-desafio
---
+++ impulso-previne/js/app.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:40,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
